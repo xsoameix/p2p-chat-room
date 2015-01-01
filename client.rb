a6df05fb4ca2e242ff9e97165f6d1bb06b35f32f
Aercus <hsien0917@gmail.com>
2015-01-01 23:17:18 +0800

    chord ver.

--- client.rb
+++ client.rb
@@ -1,1 +1,68 @@
-puts 'hello world'
+require "socket"
+require "./server"
+
+class Client
+
+  def initialize(host, port)
+    @server = Server.new host, port.to_i
+    @alive = true
+  end
+
+  def start()
+    puts "Join a chat(y/n)?"
+    cmd = $stdin.gets.chomp
+    if cmd == 'y'
+      puts "IP:"
+      ip = $stdin.gets.chomp
+      puts "port:"
+      port = $stdin.gets.chomp
+      @server.join ip, port
+    end
+    @server.start
+
+    puts "Enter the username:"
+    @name = $stdin.gets.chomp
+    @server.send_msg "#{@name} join the chat."
+    puts "================================"
+    puts "Welcome to the p2p chat room!"
+    puts "================================"
+
+    listen
+  end
+
+  def listen()
+    while @alive do
+      msg = $stdin.gets.chomp
+      cmd = msg.split
+      case cmd[0]
+      when "/table"
+        list_table
+      when "/exit"
+      	@server.leave
+      	@alive = false
+      else
+        @server.send_msg "#{@name}> #{msg}"
+      end
+    end
+  end
+
+  def list_table()
+  	id = @server.get_my_id
+    succ = @server.get_successor
+    pred = @server.get_predecessor
+    fingers = @server.get_fingers
+
+    puts "------------------"
+    puts "id: #{id}"
+    puts "successor: #{succ}"
+    puts "predecessor: #{pred}"
+    fingers.each { |x| puts x }
+    puts "------------------"
+  end
+
+end
+
+ip = ARGV[0]
+port = ARGV[1].to_i
+p2p_client = Client.new ip, port
+p2p_client.start
